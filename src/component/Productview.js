0fb0e29bdbbaa48450e884afd13757a6ee076ce8
Thanh Sơn <thanh.son@rabiloo.com>
2022-07-21 09:04:18 +0700

    update

--- src/component/Productview.js
+++ src/component/Productview.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react'
 
 function Productview() {
 
+	const [data , setData] = useState([]);
+	const [filter, setFilter] = useState(data);
 	const [posts, setPosts] = useState([])
 
 	useEffect(() => {
@@ -13,6 +15,11 @@ function Productview() {
 			})
 	}, [])
 
+
+	const filterProduct = (cat) => {
+		const updatedList = data.filter((x) => x.categories === cat);
+		setFilter(updatedList);
+	}
 
 	return (
 		<section className="bg0 p-t-23 p-b-140">
@@ -25,27 +32,27 @@ function Productview() {
 
 				<div className="flex-w flex-sb-m p-b-52">
 					<div className="flex-w flex-l-m filter-tope-group m-tb-10">
-						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
+						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" onClick={() => setFilter(data)}>
 							All Products
 						</button>
 
-						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".women">
+						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" onClick={() => filterProduct("women")}>
 							Women
 						</button>
 
-						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".men">
+						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" onClick={() => filterProduct("men")}>
 							Men
 						</button>
 
-						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".bag">
+						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" onClick={() => filterProduct("bag")}>
 							Bag
 						</button>
 
-						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".shoes">
+						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" onClick={() => filterProduct("shoes")}>
 							Shoes
 						</button>
 
-						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".watches">
+						<button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" onClick={() => filterProduct("watches")}>
 							Watches
 						</button>
 					</div>
@@ -65,24 +72,33 @@ function Productview() {
 					</div>
 				</div>
 
-				{posts.map((products) => {
-					return (
-						<>
-							<div class="container-fluid">
-								<div className='d-flex flex-row col-sm-3'>
-									<div class="card" >
-										<img src={products.imageURL} class="card-img-top" alt="..." />
-										<div class="card-body">
-											<h5 class="card-title">{products.title}</h5>
-											<p class="card-text">${products.price}</p>
-											<a href="/" class="btn btn-primary">Go somewhere</a>
+				<div className='flex-w flex-sb-m'>
+					{posts.map((products) => {
+						return (
+							<>
+								<div className='col-sm-3 mb-4'>
+									<div className="card border-0 " >
+										<div className="img-hover-zoom img-hover-zoom--colorize">
+											<img src={products.imageURL} class="w-100" alt="..."  />
 										</div>
+										<div className="card-body ">
+											<h5 className="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6 pl-1">{products.title}</h5>
+											<p className="stext-105 cl3">${products.price}</p>
+											<div className="block2-txt-child2 flex-r p-t-3">
+												<a href="/" className="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+													<img class="icon-heart1 dis-block trans-04" src="asset/images/icons/icon-heart-01.png" alt="ICON" />
+													<img class="icon-heart2 dis-block trans-04 ab-t-l" src="asset/images/icons/icon-heart-02.png" alt="ICON" />
+												</a>
+											</div>
+										</div>
+
 									</div>
 								</div>
-							</div>
-						</>
-					)
-				})}
+							</>
+						)
+					})}
+
+				</div>
 
 				<div className="flex-c-m flex-w w-full p-t-45">
 					<a href="/" className="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,6 @@ function Home() {
         <Sliders />
         <Banner />
         <Productview />
-        <Footer />
     </div>
   )
 }
